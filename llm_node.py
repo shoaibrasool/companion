ebf93a50c3e29a0cc701ddcfe0e519f1eb83efd0
shoaibrasool <shooaib6@gmail.com>
2026-07-24 03:21:08 +0500

    added system prompt

--- llm_node.py
+++ llm_node.py
@@ -1,15 +1,18 @@
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from state import CompanionState
-from langchain_core.messages import AIMessage
+from langchain_core.messages import AIMessage, SystemMessage
+from system_prompt import SystemPrompt
 
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(model = "gemini-3.1-flash-lite")
 
 def llm_node(state: CompanionState):
+    messages = [SystemMessage(content=SystemPrompt)] + state["messages"]
+    
     full_response = ""
-    for chunk in llm.stream(state["messages"]):
+    for chunk in llm.stream(messages):
         content = chunk.content
         for block in content:
             text = block.get("text", "")
