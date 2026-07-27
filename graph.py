156f7a064e34bc8e7d351a64f4bdf3dd2d7c4943
shoaibrasool <shooaib6@gmail.com>
2026-07-27 07:53:52 +0500

    Integrated Fast API in to the langgraph app. Replaced the input node and conditional edge with fast Api's websocket for realtime conversation

--- graph.py
+++ graph.py
@@ -1,32 +1,14 @@
 from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.memory import MemorySaver
 from state import CompanionState
 from llm_node import llm_node
-from input_node import input_node
 from persist_data import persist_data_node
-from condition import should_continue
-import models
-from database import init_db
-import os
-from load_chat_history import load_chat_history
 
-if not os.path.exists("sql_app.db"):
-    print("creating sql_lite db file")
-    init_db()
+builder = StateGraph(CompanionState)
+builder.add_node(llm_node)
+builder.add_node(persist_data_node)
+builder.add_edge(START, "llm_node")
+builder.add_edge("llm_node", "persist_data_node")
+builder.add_edge("persist_data_node", END)
 
-graph = StateGraph(CompanionState)
-graph.add_node(input_node)
-graph.add_node(llm_node)
-graph.add_node(persist_data_node)
-graph.add_edge(START, "input_node")
-graph.add_conditional_edges("input_node", should_continue , ["llm_node", END])
-graph.add_edge("llm_node", "persist_data_node")
-graph.add_edge("persist_data_node", "input_node")
-
-graph = graph.compile()
-
-chat_history = load_chat_history() 
-
-graph.invoke({
-    "quit": False,
-    "messages": chat_history
-})
+graph = builder.compile(checkpointer=MemorySaver())
--- llm_node.py
+++ llm_node.py
@@ -3,20 +3,24 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from state import CompanionState
 from langchain_core.messages import AIMessage, SystemMessage
 from system_prompt import SystemPrompt
+from langgraph.types import StreamWriter
 
 load_dotenv()
 
-llm = ChatGoogleGenerativeAI(model = "gemini-3.1-flash-lite")
+llm = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite")
 
-def llm_node(state: CompanionState):
+async def llm_node(state: CompanionState, writer: StreamWriter):
     messages = [SystemMessage(content=SystemPrompt)] + state["messages"]
-    
+
     full_response = ""
-    for chunk in llm.stream(messages):
+    async for chunk in llm.astream(messages):
         content = chunk.content
-        for block in content:
-            text = block.get("text", "")
-            print((text), end="", flush=True)
+        if isinstance(content, list):
+            text = "".join(b.get("text", "") for b in content)
+        else:
+            text = content or ""
+        if text:
+            writer({"type": "token", "content": text})
             full_response += text
-    print()    
-    return {"messages" : [AIMessage(content = full_response)]}
+
+    return {"messages": [AIMessage(content=full_response)]}
